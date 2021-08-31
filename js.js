811122b1f8e5219d6901de125ba63957b989f933
Nekrasov-A-A <56222414+Nekrasov-A-A@users.noreply.github.com>
2021-08-31 14:26:24 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,38 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2022';
+
+function countdown () {
+	const newYearsDate = new Date (newYears)
+	const currentDate =  new Date()
+
+
+	const totalSeconds = (newYearsDate - currentDate) / 1000
+
+	const days = Math.floor(totalSeconds /3600/24)
+	const hours = Math.floor(totalSeconds/3600) %24
+	const mins = Math.floor(totalSeconds / 60)  % 60
+	const seconds = Math.floor(totalSeconds % 60 )
+	
+	daysEl.innerHTML = days
+	hoursEl.innerHTML = hours
+	minsEl.innerHTML = mins
+	secondsEl.innerHTML = seconds
+}
+countdown()
+setInterval(countdown, 1000)
+
+function createSnow () {
+	const snowFlake = document.createElement('i');
+	snowFlake.classList.add('fas')
+	snowFlake.classList.add('fa-snowflake')
+	snowFlake.style.left = Math.random() * window.innerWidth + 'px'
+	snowFlake.style.animationDuration = Math.random() *2 +3 +'s'
+
+	document.body.appendChild(snowFlake)
+	setTimeout(() =>snowFlake.remove(),5000)
+}
+setInterval(createSnow,100)
